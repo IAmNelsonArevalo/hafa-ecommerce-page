c46ae2bd73560e0b75ef5f045c2b12a3ecaee87a
Nelson Arevalo <nelson.arevalo2119@gmail.com>
2022-09-25 19:23:43 -0500

    Full Header

---
+++ src/app/models/interfaces/views/components/header.interfaces.ts
@@ -0,0 +1,15 @@
+export interface ISsocialNetworks {
+  name: string;
+  url: string;
+}
+
+export interface IRoutesMenu {
+  path: string;
+  name: string;
+}
+
+export interface IUser {
+  fullname: string;
+  email: string;
+  phone: string;
+}
